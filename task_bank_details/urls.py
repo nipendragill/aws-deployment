3fd9d3c5e0cd8e6806202cac51049214ef10eacb
nipendragill <nipendra9457@gmail.com>
2019-09-04 01:28:53 +0530

    string query param url for ifsc code url

--- task_bank_details/urls.py
+++ task_bank_details/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from .views import CreateUserAPIView, authenticate_user, BankAPIView, BranchesView, BankBranchDetails
-
+from .views import CreateUserAPIView, authenticate_user, BankAPIView, BranchesView, BankBranchDetails, BankIFSCDetails
 urlpatterns = [
     url(r'^create/$', CreateUserAPIView.as_view(), name='create_user'),
     url(r'^get_token/$', authenticate_user, name='get_token'),
     url(r'^bank/$', BankAPIView.as_view(), name='bank_info'),
     url(r'^branches', BranchesView.as_view(), name='add_branch'),
+    url(r'^banks/(?P<ifsc_code>[\w-]+)/?$', BankIFSCDetails.as_view(), name = 'ifsc_bank_info'),
     url(r'^banks/(?P<bank_name>[\w-]+)/city_name/(?P<city_name>[\w-]+)/?$', BankBranchDetails.as_view(), name = 'get_ifsc_bank_details')
 ]
